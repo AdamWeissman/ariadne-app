c823b4cfbec36c7caab2436e18dc26560e909423
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-01-12 21:29:35 -0500

    building out the session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,8 @@ class UsersController < ApplicationController
     @user = User.find_by(email: params[:email]) # it's looking up the email key and passing the param entered in the email
     # Authenticate (not the same as validate) - through email/password combo
     if @user.authenticate(params[:password])
-
-
+      session[:work_hard] = @user.id
+      redirect "users/#{@user.id}"
     else
 
     end
@@ -23,6 +23,11 @@ class UsersController < ApplicationController
 
   get '/signup' do
     erb :signup
+  end
+
+  #user SHOW route
+  get '/users/:id' do
+    "this will be the users show route"
   end
 
 end
