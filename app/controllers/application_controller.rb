900026bcdb9c89bdb29b393334c7754f7544d81c
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-01-16 00:16:21 -0500

    added if logged in and no access to everything in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -104,8 +104,6 @@ class ApplicationController < Sinatra::Base
     end
   end
 
-#EVERYTHING ABOVE THIS LINE SORT OF MOSTLY WORKS...
-
 # CONTACT is only available if you are logged in
   get '/contact' do
     if logged_in?
@@ -116,79 +114,149 @@ class ApplicationController < Sinatra::Base
     end
   end
 
+#EVERYTHING ABOVE THIS LINE SORT OF MOSTLY WORKS...
+
+  get '/no_access'
+    erb :'/outside_the_maze/minotaur2'
+  end
+
 # PROJECT STUFF MAIN MENU
   get '/projects' do
-    erb :'/inside_the_maze/adventures/projects_home'
+    if logged_in?
+      erb :'/inside_the_maze/adventures/projects_home'
+    else
+      redirect 'no_access'
+    end
   end
 
   get '/create_a_new_project' do
-    erb :'/inside_the_maze/adventures/new_project'
+    if logged_in?
+      erb :'/inside_the_maze/adventures/new_project'
+    else
+      redirect '/no_access'
+    end
   end
 
   get '/edit_a_project' do
-    erb :'/inside_the_maze/adventures/edit_a_project'
+    if logged_in?
+      erb :'/inside_the_maze/adventures/edit_a_project'
+    else
+      redirect '/no_access'
+    end
   end
 
   get '/delete_a_project' do
-    erb :'/inside_the_maze/adventures/delete_a_project'
+    if logged_in?
+      erb :'/inside_the_maze/adventures/delete_a_project'
+    else
+      redirect '/no_access'
+    end
   end
 
   get '/delete_a_project_for_real' do
-    erb :'/inside_the_maze/adventures/delete_a_project_for_real'
+    if logged_in?
+      erb :'/inside_the_maze/adventures/delete_a_project_for_real'
+    else
+      redirect '/no_access'
+    end
   end
 
 # PROJECT STUFF PHASE I (NEW PROJECT) -- starts with create_a_new_project
 
   get '/phase_1_preview' do
-    erb :'/inside_the_maze/adventures/phase_1/the_phase_1_preview'
+    if logged_in?
+      erb :'/inside_the_maze/adventures/phase_1/the_phase_1_preview'
+    else
+      redirect '/no_access'
+    end
   end
 
   get '/phase_1_saved' do
-    erb :'/inside_the_maze/adventures/phase_1/phase_1_complete_with_data'
+    if logged_in?
+      erb :'/inside_the_maze/adventures/phase_1/phase_1_complete_with_data'
+    else
+      redirect '/no_access'
+    end
   end
 
   get '/phase_1_preview_something_sucked_let_me_fix_it' do
-    erb :'/inside_the_maze/adventures/phase_1/phase_1_fix_the_preview'
+    if logged_in?
+      erb :'/inside_the_maze/adventures/phase_1/phase_1_fix_the_preview'
+    else
+      redirect '/no_access'
+    end
   end
 
 # PHASE II
 
   get '/phase_2' do
-    erb :'/inside_the_maze/adventures/phase_2/phase_2_first_iteration'
+    if logged_in?
+      erb :'/inside_the_maze/adventures/phase_2/phase_2_first_iteration'
+    else
+      redirect '/no_access'
+    end
   end
 
   get '/phase_2_saved' do
-    erb :'/inside_the_maze/adventures/phase_2/phase_2_complete_with_data'
+    if logged_in?
+      erb :'/inside_the_maze/adventures/phase_2/phase_2_complete_with_data'
+    else
+      redirect '/no_access'
+    end
   end
 
 # PHASE III
 
   get '/phase_3' do
-    erb :'/inside_the_maze/adventures/phase_3/phase_3_first_iteration'
+    if logged_in?
+      erb :'/inside_the_maze/adventures/phase_3/phase_3_first_iteration'
+    else
+      redirect '/no_access'
+    end
   end
 
   get '/phase_3_saved' do
-    erb :'/inside_the_maze/adventures/phase_3/phase_3_complete_with_data'
+    if logged_in?
+      erb :'/inside_the_maze/adventures/phase_3/phase_3_complete_with_data'
+    else
+      redirect '/no_access'
+    end
   end
 
   # PHASE IV
 
   get '/phase_4' do
-    erb :'/inside_the_maze/adventures/phase_4/phase_4_first_iteration'
+    if logged_in?
+      erb :'/inside_the_maze/adventures/phase_4/phase_4_first_iteration'
+    else
+      redirect '/no_access'
+    end
   end
 
   get '/phase_4_saved' do
-    erb :'/inside_the_maze/adventures/phase_4/phase_4_complete_with_data'
+    if logged_in?
+      erb :'/inside_the_maze/adventures/phase_4/phase_4_complete_with_data'
+    else
+      redirect '/no_access'
+    end
   end
 
   # PHASE V YOU ARE HERE...
 
   get '/phase_5' do
-    erb :'/inside_the_maze/adventures/phase_5/phase_5_first_iteration'
+    if logged_in?
+      erb :'/inside_the_maze/adventures/phase_5/phase_5_first_iteration'
+    else
+      redirect '/no_access'
+    end
   end
 
   get '/phase_5_saved' do
-    erb :'/inside_the_maze/adventures/phase_5/phase_5_complete_with_data'
+    if logged_in?
+      erb :'/inside_the_maze/adventures/phase_5/phase_5_complete_with_data'
+    else
+      redirect '/no_access'
+    end
   end
 
 end
