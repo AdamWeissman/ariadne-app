0d55c1116f123ab6967da5916920f18d0af71d54
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-01-21 07:42:08 -0500

    intuition that something will crash... CRUD developing with extended phase 1 parsing

--- app/controllers/phase_one_controller.rb
+++ app/controllers/phase_one_controller.rb
@@ -1,26 +1,33 @@
 class PhaseOneController < ApplicationController
 
-  patch '/update_in_place/:id' do
+  get '/phase_1_preview/:id' do
+    if logged_in?
+      @user = current_user
+      @project = Project.find(params[:id])
+      erb :'/inside_the_maze/adventures/phase_1/the_phase_1_preview'
+    else
+      redirect '/no_access'
+    end
+  end
+
+  get '/phase_1_preview_something_sucked_let_me_fix_it/:id' do
+    if logged_in?
+      @user = current_user
+      @project = Project.find(params[:id])
+      erb :'/inside_the_maze/adventures/phase_1/phase_1_fix_the_preview'
+    else
+      redirect '/no_access'
+    end
+  end
+
+  patch '/phase_1_preview/:id' do
     @user = current_user
     @project = Project.find(params[:id])
-    #@project = Project.find_by(user: params[:user_id], id: params[:project_id])
     @project.project_name = params[:project_name]
     @project.project_summary = params[:project_summary]
     @project.the_initial_blob_to_parse = params[:the_initial_blob_to_parse]
     @project.save
     erb :'/inside_the_maze/adventures/phase_1/the_phase_1_preview'
-    #redirect "/phase_1_preview/#{@project.id}"
-  end
-
-  get '/phase_1_preview/:id' do
-    if logged_in?
-      @user = current_user
-      @project = Project.find(params[:id])
-      #@project_parsed = @project.the_initial_blob_to_parse.upcase!
-      erb :'/inside_the_maze/adventures/phase_1/the_phase_1_preview'
-    else
-      redirect '/no_access'
-    end
   end
 
   post '/phase_1_saved/:id' do
@@ -36,14 +43,7 @@ class PhaseOneController < ApplicationController
     end
   end
 
-#below this line currently does not work.
-
-  get '/phase_1_preview_something_sucked_let_me_fix_it' do
-    if logged_in?
-      erb :'/inside_the_maze/adventures/phase_1/phase_1_fix_the_preview'
-    else
-      redirect '/no_access'
-    end
-  end
+#will need to do an update in place if someone wants to add a task directly before comment sections
+#that means adding a new patch, a new gets, a new post for the final comment...
 
 end
