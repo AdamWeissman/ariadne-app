daa8cf9702c6c89fa506009dbf777b3966f158b8
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-01-21 08:52:16 -0500

    not all hidden values are passing from preview to complete, also inputs that dont change should not be alterable

--- app/controllers/phase_one_controller.rb
+++ app/controllers/phase_one_controller.rb
@@ -20,6 +20,16 @@ class PhaseOneController < ApplicationController
     end
   end
 
+  get '/phase_1_saved/:id' do
+    if logged_in?
+      @user = current_user
+      @project = Project.find(params[:id])
+      erb :'/inside_the_maze/adventures/phase_1/phase_1_complete_with_data'
+    else
+      redirect '/no_access'
+    end
+  end
+
   patch '/phase_1_preview/:id' do
     @user = current_user
     @project = Project.find(params[:id])
@@ -30,18 +40,15 @@ class PhaseOneController < ApplicationController
     erb :'/inside_the_maze/adventures/phase_1/the_phase_1_preview'
   end
 
-  post '/phase_1_saved/:id' do
-    if logged_in?
-      @user = current_user
-      @project = Project.find(params[:id])
-      @project.project_name = params[:project_name]
-      @project.project_summary = params[:project_summary]
-      @project.the_initial_blob_to_parse = params[:the_initial_blob_to_parse]
-      erb :'/inside_the_maze/adventures/phase_1/phase_1_complete_with_data'
-    else
-      redirect '/no_access'
-    end
+  patch '/phase_1_saved/:id' do
+    @user = current_user
+    @project = Project.find(params[:id])
+    @project.project_name = params[:project_name]
+    @project.project_summary = params[:project_summary]
+    @project.save
+    erb :'/inside_the_maze/adventures/phase_1/phase_1_complete_with_data'
   end
+
 
 #will need to do an update in place if someone wants to add a task directly before comment sections
 #that means adding a new patch, a new gets, a new post for the final comment...
