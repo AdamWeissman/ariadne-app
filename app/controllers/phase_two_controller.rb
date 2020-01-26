fc54efed826ab815aceda638df0089847e4c9863
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-01-25 21:11:06 -0500

    patch still not working but at least not crashing

--- app/controllers/phase_two_controller.rb
+++ app/controllers/phase_two_controller.rb
@@ -7,6 +7,7 @@ class PhaseTwoController < ApplicationController
       @task = Task.find_by(project_id: params[:project_id], comment_or_measure: "you must change this to continue.")
       if @task.blank?
         @task = Task.find_by(project_id: params[:project_id])
+        @project = Project.find(params[:project_id])
         redirect "/phase_2_question_time/#{@project.id}/#{@task.id}"
       else
         erb :'/inside_the_maze/adventures/phase_2/phase_2_first_iteration'
@@ -30,35 +31,58 @@ class PhaseTwoController < ApplicationController
     end
   end
 
+  # these two sort of work...
+
   get '/phase_2_question_time/:project_id/:task_id' do
     if logged_in?
       @user = current_user
       @project = Project.find(params[:project_id])
       @task = Task.find(params[:task_id])
-      @task_score = TaskScore.find_by(task_id: @task.id, necessary_or_optional_for_form_rendering: "unknown", quick_or_slow_for_form_rendering: "unknown", easy_or_hard_for_form_rendering: "unknown")
-    erb :'/inside_the_maze/adventures/phase_2/phase_2_question_time'
+      @task_score = TaskScore.find_by(task_id: params[:task_id], necessary_or_optional_for_form_rendering: "unknown", quick_or_slow_for_form_rendering: "unknown", easy_or_hard_for_form_rendering: "unknown")
+      erb :'/inside_the_maze/adventures/phase_2/phase_2_question_time'
+      if @task_score.blank?
+        redirect "/projects"
+      else
+        erb :'/inside_the_maze/adventures/phase_2/phase_2_question_time'
+      end
     else
       redirect '/no_access'
     end
   end
 
-#BELOW THIS LINE ARE ROUTES THAT ARE NOT YET WORKING, ABOVE THIS LINE IS WORKING BUT MAYBE WITH PROBLEMS.
+patch '/phase_2_question_time/:project_id/:task_id/:task_score_id' do
+  if logged_in?
+    @user = current_user
+    @project = Project.find(params[:project_id])
+    @task = Task.find(params[:task_id])
+    @task_score = TaskScore.find_by(params[:task_score_id])
+    @task_score.necessary_or_optional_for_form_rendering = params[:necessary_or_optional_for_form_rendering]
+    @task_score.quick_or_slow_for_form_rendering = params[:quick_or_slow_for_form_rendering]
+    @task_score.easy_or_hard_for_form_rendering = params[:easy_or_hard_for_form_rendering]
+    @task_score.save
+    redirect "/phase_2_question_time_work_around/#{@project.id}/#{@task.id}"
+  else
+    redirect '/no_access'
+  end
+end
 
-#patch '/phase_2_question_time/:project_id/:task_id' do
-#  if logged_in?
-#    @user = current_user
-#    @project = Project.find(params[:project_id])
-#    @task = Task.find(params[:task_id])
-#    @task_score = TaskScore.find_by(task_id: @task.id)
-#    @task_score.necessary_or_optional_for_form_rendering = params[:necessary_or_optional_for_form_rendering]
-#    @task_score.quick_or_slow_for_form_rendering = params[:quick_or_slow_for_form_rendering]
-#    @task_score.easy_or_hard_for_form_rendering = params[:easy_or_hard_for_form_rendering]
-#    @task_score.save
-#    @task.save
-#    redirect "/phase_2_question_time/#{@project.id}/#{@task.id}"
-#  else
-#    redirect '/no_access'
-#  end
-#end
+get '/phase_2_question_time_work_around/:project_id/:task_id' do
+  if logged_in?
+    @user = current_user
+    @project = Project.find(params[:project_id])
+    @task = Task.find(params[:task_id])
+    @task_score = TaskScore.find_by(task_id: params[:task_id], necessary_or_optional_for_form_rendering: "unknown", quick_or_slow_for_form_rendering: "unknown", easy_or_hard_for_form_rendering: "unknown")
+    erb :'/inside_the_maze/adventures/phase_2/phase_2_question_time'
+    if @task_score.blank?
+      redirect "/projects"
+    else
+      erb :'/inside_the_maze/adventures/phase_2/phase_2_question_time'
+    end
+  else
+    redirect '/no_access'
+  end
+end
+
+
 
 end
