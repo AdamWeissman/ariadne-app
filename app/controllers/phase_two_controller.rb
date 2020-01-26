82ea7352b690e382b7b92176be421299c7d5307f
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-01-26 09:05:27 -0500

    starting to see the value of github after rewriting the same lines of code some many times over... phase II controller and question time partially patching in place

--- app/controllers/phase_two_controller.rb
+++ app/controllers/phase_two_controller.rb
@@ -6,9 +6,7 @@ class PhaseTwoController < ApplicationController
       @project = Project.find(params[:project_id])
       @task = Task.find_by(project_id: params[:project_id], comment_or_measure: "you must change this to continue.")
       if @task.blank?
-        @task = Task.find_by(project_id: params[:project_id])
-        @project = Project.find(params[:project_id])
-        redirect "/phase_2_question_time"
+        redirect "/phase_2_question_time/#{@project.id}"
       else
         erb :'/inside_the_maze/adventures/phase_2/phase_2_first_iteration'
       end
@@ -33,26 +31,30 @@ class PhaseTwoController < ApplicationController
 
   # these two sort of work...
 
-  get '/phase_2_question_time' do
+  get '/phase_2_question_time/:project_id/?:the_task_id_to_eliminate_from_the_search?' do
     if logged_in?
       @user = current_user
-      @task = Task.find_by(project_id: params[:project_id])
-      @task_score = @task.TaskScore.find_by(necessary_or_optional_for_form_rendering: "unknown", quick_or_slow_for_form_rendering: "unknown", easy_or_hard_for_form_rendering: "unknown")
+      @project = Project.find(params[:project_id])
+      @task = @project.tasks.first
+      @task_score = @task.task_score
       erb :'/inside_the_maze/adventures/phase_2/phase_2_question_time'
     else
       redirect '/no_access'
     end
   end
 
-  put '/phase_2_question_time/:task_score_id' do
+  patch '/phase_2_question_time/:task_id/:project_id/:task_score_id' do
     if logged_in?
       @user = current_user
+      @task = Task.find(params[:task_id])
+      @project = Project.find(params[:project_id])
       @task_score = TaskScore.find_by(params[:task_score_id])
       @task_score.necessary_or_optional_for_form_rendering = params[:necessary_or_optional_for_form_rendering]
       @task_score.quick_or_slow_for_form_rendering = params[:quick_or_slow_for_form_rendering]
       @task_score.easy_or_hard_for_form_rendering = params[:easy_or_hard_for_form_rendering]
       @task_score.save
       erb :'/inside_the_maze/adventures/phase_2/phase_2_question_time'
+      #redirect "/phase_2_question_time/#{@project.id}/#{@task.id}"
     else
       redirect '/no_access'
     end
