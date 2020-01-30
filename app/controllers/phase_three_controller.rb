a0beb042a05839fe8c525e7a103a2d84bebbb17d
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-01-30 07:20:43 -0500

    phase 3 routes, get for primary, patch for segment submit, and get for segment view complete... data not passing yet

--- app/controllers/phase_three_controller.rb
+++ app/controllers/phase_three_controller.rb
@@ -10,4 +10,24 @@ class PhaseThreeController < ApplicationController
     end
   end
 
+  patch '/phase_3_complete_with_data/:project_id' do
+    if logged_in?
+      @user = current_user
+      @project = Project.find(params[:project_id])
+      redirect "/phase_3_complete_with_data/#{@project.id}"
+    else
+      redirect '/no_access'
+    end
+  end
+
+  get '/phase_3_complete_with_data/:project_id' do
+    if logged_in?
+      @user = current_user
+      @project = Project.find(params[:project_id])
+      erb :"/inside_the_maze/adventures/phase_3/phase_3_complete_with_data"
+    else
+      redirect '/no_access'
+    end
+  end
+
 end
