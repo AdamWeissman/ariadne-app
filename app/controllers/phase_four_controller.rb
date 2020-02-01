fc5437ed046a18ade87af022ced8e062c0bad1e0
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-02-01 05:24:16 -0500

    phase three complete with data renders correctly, but is incomplete

--- app/controllers/phase_four_controller.rb
+++ app/controllers/phase_four_controller.rb
@@ -37,7 +37,7 @@ class PhaseFourController < ApplicationController
       @project.current_phase = 3
       @project.save
 
-      redirect "/phase_3_complete_with_data/#{@project.id}"
+      redirect "/phase_4_complete_with_data/#{@project.id}"
     else
       redirect '/no_access'
     end
@@ -47,7 +47,7 @@ class PhaseFourController < ApplicationController
     if logged_in?
       @user = current_user
       @project = Project.find(params[:project_id])
-      erb :"/inside_the_maze/adventures/phase_3/phase_3_complete_with_data"
+      erb :"/inside_the_maze/adventures/phase_3/phase_4_complete_with_data"
     else
       redirect '/no_access'
     end
