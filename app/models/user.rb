98185fe5bb317baf3593bfb073ea29abe272df5e
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-02-04 23:22:14 -0500

    very limited validation for project requirement

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
   has_many :ariadne_messages
   has_many :projects
+
+
+  validates :first_name, :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i}
+
   has_secure_password
 
 end
